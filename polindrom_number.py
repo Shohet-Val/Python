00d8e447458951ad9af9bd074ddd1d566f93e34a
Shohet-Val <53824336+Shohet-Val@users.noreply.github.com>
2020-06-03 15:35:58 +0300

    Create polindrom_number.py

---
+++ polindrom_number.py
@@ -0,0 +1,18 @@
+palindrom_lst = []
+for i in range(101, 999):
+    print('i =',i)
+    for j in range(i, 999):
+        multi_IJ = str(i * j)
+        #
+        palindrom = multi_IJ[::-1]#
+        print(i, '*', j, '=', str(i * j))
+        #print(palindrom)
+        if palindrom == multi_IJ:
+            palindrom=int(palindrom)
+            print(palindrom)
+            palindrom_lst.append(palindrom)
+print(palindrom_lst)
+#количество таких чисел
+print()
+#
+print(max(palindrom_lst))
